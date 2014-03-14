a031eff928b9a5355294cfc7b56a017ed7af7bf7
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-03-14 14:33:54 -0600

    Starting to flesh out the Command Line handler.

--- src/main/java/com/spectralogic/ds3cli/Main.java
+++ src/main/java/com/spectralogic/ds3cli/Main.java
@@ -1,7 +1,20 @@
 package com.spectralogic.ds3cli;
+
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("ohai!");
+    public static void main(String[] args) throws Exception {
+        Options options = new Options();
+        options.addOption("c", false, "The Command to execute");
+        CommandLineParser parser = new BasicParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        if(cmd.hasOption("c")) {
+            System.out.println("Got a command");
+        }
     }
 }
