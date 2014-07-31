ab8aa197594ffc5641b53cdab688048bfc6c141c
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-07-31 16:19:15 -0600

    Added a prefix option for the get_bulk command.  Used the optimize imports command to remove unused imports.  Reved the version of the cli.

--- src/main/java/com/spectralogic/ds3cli/command/GetBulk.java
+++ src/main/java/com/spectralogic/ds3cli/command/GetBulk.java
@@ -15,18 +15,27 @@
 
 package com.spectralogic.ds3cli.command;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Iterables;
 import com.spectralogic.ds3cli.Arguments;
+import com.spectralogic.ds3cli.logging.Logging;
 import com.spectralogic.ds3client.Ds3Client;
 import com.spectralogic.ds3client.helpers.Ds3ClientHelpers;
 import com.spectralogic.ds3client.helpers.FileObjectGetter;
+import com.spectralogic.ds3client.models.Contents;
+import com.spectralogic.ds3client.models.bulk.Ds3Object;
+import com.spectralogic.ds3client.serializer.XmlProcessingException;
 import org.apache.commons.cli.MissingOptionException;
 
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.security.SignatureException;
 
 public class GetBulk extends CliCommand {
     private String bucketName;
     private Path outputPath;
+    private String prefix;
     public GetBulk(final Ds3Client client) {
         super(client);
     }
@@ -38,23 +47,53 @@ public class GetBulk extends CliCommand {
             throw new MissingOptionException("The bulk get command requires '-b' to be set.");
         }
 
-        final String prefix = args.getDirectory();
-        if (prefix == null || prefix.equals(".")) {
+        final String directory = args.getDirectory();
+        if (directory == null || directory.equals(".")) {
             outputPath = FileSystems.getDefault().getPath(".");
         } else {
-            final Path dirPath = FileSystems.getDefault().getPath(prefix);
+            final Path dirPath = FileSystems.getDefault().getPath(directory);
             outputPath = FileSystems.getDefault().getPath(".").resolve(dirPath);
         }
+
+        this.prefix = args.getPrefix();
         return this;
     }
 
 
     @Override
     public String call() throws Exception {
-        final Ds3ClientHelpers helper = Ds3ClientHelpers.wrap(getClient());
-        final Ds3ClientHelpers.ReadJob job = helper.startReadAllJob(bucketName);
-        job.read(new FileObjectGetter(outputPath));
-
-        return "SUCCESS: Wrote all the objects from " + bucketName + " to " + outputPath.toString();
+        if (this.prefix == null) {
+            Logging.log("Getting all objects from " + this.bucketName);
+            return restoreAll();
+        }
+        else {
+            Logging.log("Getting only those objects that start with " + this.prefix);
+            return restoreSome();
+        }
     }
+
+    private String restoreSome() throws IOException, SignatureException, XmlProcessingException {
+        final Ds3ClientHelpers helper = Ds3ClientHelpers.wrap(getClient());
+        final Iterable<Contents> contents = helper.listObjects(this.bucketName, this.prefix);
+        final Iterable<Ds3Object> objects = Iterables.transform(contents, new Function<Contents, Ds3Object>() {
+            @Override
+            public Ds3Object apply(final Contents input) {
+                return new Ds3Object(input.getKey(), input.getSize());
+            }
+        });
+
+        final Ds3ClientHelpers.ReadJob job = helper.startReadJob(this.bucketName, objects);
+        job.read(new FileObjectGetter(this.outputPath));
+        return "SUCCESS: Wrote all the objects that start with '"+ this.prefix +"' from " + this.bucketName + " to " + this.outputPath.toString();
+    }
+
+    private String restoreAll() throws XmlProcessingException, SignatureException, IOException {
+        final Ds3ClientHelpers helper = Ds3ClientHelpers.wrap(getClient());
+        final Ds3ClientHelpers.ReadJob job = helper.startReadAllJob(this.bucketName);
+        job.read(new FileObjectGetter(this.outputPath));
+
+        return "SUCCESS: Wrote all the objects from " + this.bucketName + " to " + this.outputPath.toString();
+    }
+
+
 }
