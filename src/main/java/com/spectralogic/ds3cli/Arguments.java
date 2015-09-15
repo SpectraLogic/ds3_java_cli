7e07a017ebbe53d0e0804603b94011c7f7355632
Rachel Tucker <rachelt@spectralogic.com>
2015-09-15 11:16:13 -0600

    Added -p option for put_object to prepend path to object name

--- src/main/java/com/spectralogic/ds3cli/Arguments.java
+++ src/main/java/com/spectralogic/ds3cli/Arguments.java
@@ -83,7 +83,7 @@ public class Arguments {
         directory.setArgName("directory");
         final Option objectName = new Option("o", true, "The name of the object to be retrieved or stored");
         objectName.setArgName("objectFileName");
-        final Option prefix = new Option("p", true, "Restores only objects who's names start with prefix.  Used with `get_bulk`");
+        final Option prefix = new Option("p", true, "Restores only objects who's names start with prefix.  Used with `get_bulk` and 'put_object'");
         prefix.setArgName("prefix");
         final Option proxy = new Option("x", true, "The URL of the proxy server to use or have \"http_proxy\" set as an environment variable");
         proxy.setArgName("proxy");
