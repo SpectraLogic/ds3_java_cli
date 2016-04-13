c146f542736d4ed455386ad5eb0cdfe0e1d0a0eb
berkeleysquare <johnk@spectralogic.com>
2016-04-13 11:40:15 -0600

    Fix get_tapes command (JAVACLI-68); new build obviates JAVACLI-62

---
+++ ds3_java_cli/src/main/java/com/spectralogic/ds3cli/views/cli/GetTapesView.java
@@ -0,0 +1,75 @@
+/*
+ * ******************************************************************************
+ *   Copyright 2014-2015 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+package com.spectralogic.ds3cli.views.cli;
+
+import com.bethecoder.ascii_table.ASCIITable;
+import com.bethecoder.ascii_table.ASCIITableHeader;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.spectralogic.ds3cli.View;
+import com.spectralogic.ds3cli.models.GetTapesResult;
+import com.spectralogic.ds3cli.models.GetTapesWithFullDetailsResult;
+import com.spectralogic.ds3client.models.NamedDetailedTape;
+import com.spectralogic.ds3client.models.NamedDetailedTapeList;
+import com.spectralogic.ds3client.models.Tape;
+import com.spectralogic.ds3client.models.TapeList;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.TimeZone;
+
+import static com.spectralogic.ds3cli.util.Utils.nullGuard;
+import static com.spectralogic.ds3cli.util.Utils.nullGuardToString;
+
+public class GetTapesView implements View<GetTapesResult> {
+    @Override
+    public String render(final GetTapesResult obj) throws JsonProcessingException {
+        if (obj != null) {
+            final TapeList tapeList = obj.getTapeList();
+            if ((tapeList != null) || (null != tapeList.getTapes())) {
+                return ASCIITable.getInstance().getTable(getHeaders(), formatTapeList(tapeList));
+            }
+        }
+        return "You do not have any tapes";
+    }
+
+    private String[][] formatTapeList(final TapeList tapeList) {
+        final List<Tape> tapes = tapeList.getTapes();
+        final String [][] formatArray = new String[tapes.size()][];
+
+        int i = 0;
+        for (final Tape tape : tapes ) {
+            final String [] bucketArray = new String[5];
+            bucketArray[0] = nullGuardToString(tape.getBarCode());
+            bucketArray[1] = nullGuardToString(tape.getId());
+            bucketArray[2] = nullGuardToString(tape.getState());
+            bucketArray[3] = nullGuardToString(tape.getLastModified());
+            bucketArray[4] = nullGuardToString(tape.getAvailableRawCapacity());
+            formatArray[i++] = bucketArray;
+        }
+        return formatArray;
+    }
+
+    private ASCIITableHeader[] getHeaders() {
+        return new ASCIITableHeader[]{
+                new ASCIITableHeader("Bar Code", ASCIITable.ALIGN_LEFT),
+                new ASCIITableHeader("ID", ASCIITable.ALIGN_LEFT),
+                new ASCIITableHeader("State", ASCIITable.ALIGN_LEFT),
+                new ASCIITableHeader("Last Modified", ASCIITable.ALIGN_LEFT),
+                new ASCIITableHeader("Available Raw Capacity", ASCIITable.ALIGN_LEFT)
+        };
+    }
+}
+
---
+++ ds3_java_cli/src/main/java/com/spectralogic/ds3cli/views/json/GetTapesView.java
@@ -0,0 +1,40 @@
+/*
+ * ******************************************************************************
+ *   Copyright 2014-2015 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+package com.spectralogic.ds3cli.views.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.spectralogic.ds3cli.View;
+import com.spectralogic.ds3cli.models.GetTapesResult;
+import com.spectralogic.ds3cli.models.GetTapesWithFullDetailsResult;
+import com.spectralogic.ds3cli.util.JsonMapper;
+import com.spectralogic.ds3client.models.NamedDetailedTapeList;
+import com.spectralogic.ds3client.models.TapeList;
+
+public class GetTapesView implements View<GetTapesResult> {
+    @Override
+    public String render(final GetTapesResult obj) throws JsonProcessingException {
+        final TapeList result = obj.getTapeList();
+        final CommonJsonView view = CommonJsonView.newView(CommonJsonView.Status.OK);
+
+        if ((result == null) || (null == result.getTapes())) {
+            view.message("You do not have any tapes");
+            return JsonMapper.toJson(view);
+        }
+
+        return JsonMapper.toJson(view.data(result));
+    }
+}
+
