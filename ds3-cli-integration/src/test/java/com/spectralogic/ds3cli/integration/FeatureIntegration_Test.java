59edb1e9e73327b4add57f21c111c609b1b3dc8d
DenverM80 <denverm@spectralogic.com>
2016-08-03 15:33:17 -0600

    remove extra print

--- ds3-cli-integration/src/test/java/com/spectralogic/ds3cli/integration/FeatureIntegration_Test.java
+++ ds3-cli-integration/src/test/java/com/spectralogic/ds3cli/integration/FeatureIntegration_Test.java
@@ -543,7 +543,6 @@ public class FeatureIntegration_Test {
 
             final Arguments args = new Arguments(new String[]{"--http", "-c", "get_physical_placement", "-b", bucketName, "-o", "beowulf.txt" });
             final CommandResponse response = Util.command(client, args);
-            System.out.println(response.getMessage());
             assertTrue(response.getMessage().contains("| Object Name |                  ID                  | In Cache | Length | Offset | Latest | Version |"));
             assertTrue(response.getMessage().contains("| beowulf.txt |"));
             assertTrue(response.getMessage().contains("| true     | 294059 | 0      | true   | 1       |"));
