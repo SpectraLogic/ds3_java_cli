377010308d3d4ed24bdb7772fe9a438013e1a6e3
unknown <johnk@spectralogic.com>
2016-09-27 14:55:53 -0600

    remove inheritance, build CSV directly, not from String[][]

--- ds3_java_cli/src/main/java/com/spectralogic/ds3cli/views/csv/DetailedObjectsPhysicalView.java
+++ ds3_java_cli/src/main/java/com/spectralogic/ds3cli/views/csv/DetailedObjectsPhysicalView.java
@@ -16,54 +16,83 @@
 package com.spectralogic.ds3cli.views.csv;
 
 import com.google.common.collect.ImmutableList;
+import com.spectralogic.ds3cli.View;
 import com.spectralogic.ds3cli.models.GetDetailedObjectsResult;
 import com.spectralogic.ds3client.models.*;
 import com.spectralogic.ds3client.utils.Guard;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
 
 import static com.spectralogic.ds3cli.util.Utils.*;
 
-public class DetailedObjectsPhysicalView extends CsvView<GetDetailedObjectsResult> {
+public class DetailedObjectsPhysicalView implements View<GetDetailedObjectsResult> {
     private ImmutableList<DetailedS3Object> objects;
+
+    private static final List HEADERS = ImmutableList.of("Name", "Bucket", "Owner", "Size", "Type", "Creation Date", "Barcode", "State");
+    private static final int COLUMN_COUNT = HEADERS.size();
+    protected static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+    static {
+        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
 
     @Override
     public String render(final GetDetailedObjectsResult obj) {
         if (obj == null || (obj.getObjIterator() == null) || !obj.getObjIterator().iterator().hasNext()) {
             return "No objects returned";
         }
-
         objects = ImmutableList.copyOf(obj.getObjIterator());
-        initTable(ImmutableList.of("Name", "Bucket", "Owner", "Size", "Type", "Creation Date", "Barcode", "State"));
-
         return renderTable();
     }
 
-    @Override
-    protected String[][] formatTableContents() {
-        final ArrayList<String[]> formatArray = new ArrayList<String[]>();
+    public String renderTable()  {
+        final Appendable outs = new StringWriter();
+        try {
+            final CSVPrinter csv = new CSVPrinter(outs, CSVFormat.EXCEL);
+            csv.printRecord(HEADERS);
+            for (final DetailedS3Object object : objects) {
+                for(String[] line : formatTableRows(object)) {
+                    csv.printRecord((Object[])line);
+                }
+            }
+            csv.flush();
+            csv.close();
+        } catch (final IOException e) {
+            return "ERROR: Failed to create CSV output";
+        }
+        return outs.toString();
+    }
+
+    protected String[][] formatTableRows(final DetailedS3Object detailedObject) {
+        // one line for each instance on tape -- mine down to get Physical Placement
+        final List<String[]> formatArray = new  ArrayList<String[]>();
         int lineCount = 0;
-        for (final DetailedS3Object detailedObject : this.objects) {
-            // one line for each instance on tape -- mine down to get Physical Placement
-            if((detailedObject.getBlobs() != null) && !Guard.isNullOrEmpty(detailedObject.getBlobs().getObjects())) {
-                for (final BulkObject object : detailedObject.getBlobs().getObjects()) {
-                    if (object.getPhysicalPlacement() != null) {
-                        if (!Guard.isNullOrEmpty(object.getPhysicalPlacement().getTapes())) {
-                            for (final Tape tape : object.getPhysicalPlacement().getTapes()) {
-                                formatArray.add(writeLine(tape, detailedObject));
-                                lineCount++;
-                            } // for tapes
-                        } // if tapes
-                    } // if physical placement
-                } // for objects
-            } // if
-        } // for DetailedS3Object
-        final String[][] ret = new String[lineCount][this.columnCount];
+        if((detailedObject.getBlobs() != null) && !Guard.isNullOrEmpty(detailedObject.getBlobs().getObjects())) {
+            for (final BulkObject object : detailedObject.getBlobs().getObjects()) {
+                if (object.getPhysicalPlacement() != null) {
+                    if (!Guard.isNullOrEmpty(object.getPhysicalPlacement().getTapes())) {
+                        for (final Tape tape : object.getPhysicalPlacement().getTapes()) {
+                            formatArray.add(writeLine(tape, detailedObject));
+                            lineCount++;
+                        } // for tapes
+                    } // if tapes
+                } // if physical placement
+            } // for objects
+        } // if
+        final String[][] ret = new String[lineCount][COLUMN_COUNT];
         return formatArray.toArray(ret);
     }
 
     private String[] writeLine(final Tape tape, final DetailedS3Object detailedObject) {
-        final String[] tapeArray = new String[this.columnCount];
+        final String[] tapeArray = new String[COLUMN_COUNT];
         tapeArray[0] = nullGuard(detailedObject.getName());
         tapeArray[1] = nullGuardToString(detailedObject.getBucketId());
         tapeArray[2] = nullGuardToString(detailedObject.getOwner());
--- ds3_java_cli/src/main/java/com/spectralogic/ds3cli/views/csv/DetailedObjectsView.java
+++ ds3_java_cli/src/main/java/com/spectralogic/ds3cli/views/csv/DetailedObjectsView.java
@@ -17,54 +17,73 @@ package com.spectralogic.ds3cli.views.csv;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
+import com.spectralogic.ds3cli.View;
 import com.spectralogic.ds3cli.models.GetDetailedObjectsResult;
 import com.spectralogic.ds3client.models.*;
 import com.spectralogic.ds3client.utils.Guard;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 import static com.spectralogic.ds3cli.util.Utils.*;
 
-public class DetailedObjectsView extends CsvView<GetDetailedObjectsResult> {
+public class DetailedObjectsView implements View<GetDetailedObjectsResult> {
 
     private static final String TAPE_SEPARATOR = " | ";
 
     private Iterable<DetailedS3Object> objects;
+    private static final List<String> HEADERS = ImmutableList.of("Name", "Bucket", "Owner", "Size", "Type", "Creation Date", "Tapes", "Pools");
+    private static final int COLUMN_COUNT = HEADERS.size();
+    protected static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+    static {
+        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
 
     @Override
     public String render(final GetDetailedObjectsResult obj) {
         if (obj == null || (obj.getObjIterator() == null) || !obj.getObjIterator().iterator().hasNext()) {
             return "No objects returned";
         }
-
         objects = obj.getObjIterator();
-        initTable(ImmutableList.of("Name", "Bucket", "Owner", "Size", "Type", "Creation Date", "Tapes", "Pools"));
-
         return renderTable();
     }
 
-    @Override
-    protected String[][] formatTableContents() {
-        final ArrayList<String[]> formatArray = new ArrayList<String[]>();
-        int lineCount = 0;
-        for (final DetailedS3Object detailedObject : this.objects) {
-            final String [] bucketArray = new String[this.columnCount];
-            bucketArray[0] = nullGuard(detailedObject.getName());
-            bucketArray[1] = nullGuardToString(detailedObject.getBucketId());
-            bucketArray[2] = nullGuardToString(detailedObject.getOwner());
-            bucketArray[3] = nullGuardToString(detailedObject.getSize());
-            bucketArray[4] = nullGuardToString(detailedObject.getType());
-            bucketArray[5] = nullGuardToDate(detailedObject.getCreationDate(), DATE_FORMAT);
-            bucketArray[6] = concatenateTapes(detailedObject.getBlobs());
-            bucketArray[7] = concatenatePools(detailedObject.getBlobs());
-            formatArray.add(bucketArray);
-            lineCount++;
-        }
-        final String[][] ret = new String[lineCount][this.columnCount];
-        return formatArray.toArray(ret);
+    protected String[] formatTableRow(final DetailedS3Object detailedObject) {
+        final String [] formatArray = new String[COLUMN_COUNT];
+        formatArray[0] = nullGuard(detailedObject.getName());
+        formatArray[1] = nullGuardToString(detailedObject.getBucketId());
+        formatArray[2] = nullGuardToString(detailedObject.getOwner());
+        formatArray[3] = nullGuardToString(detailedObject.getSize());
+        formatArray[4] = nullGuardToString(detailedObject.getType());
+        formatArray[5] = nullGuardToDate(detailedObject.getCreationDate(), DATE_FORMAT);
+        formatArray[6] = concatenateTapes(detailedObject.getBlobs());
+        formatArray[7] = concatenatePools(detailedObject.getBlobs());
+        return formatArray;
     }
-    
+
+    public String renderTable()  {
+        final Appendable outs = new StringWriter();
+        try {
+            final CSVPrinter csv = new CSVPrinter(outs, CSVFormat.EXCEL);
+            csv.printRecord(HEADERS);
+            for (final DetailedS3Object line : objects) {
+                csv.printRecord((Object[])formatTableRow(line));
+            }
+            csv.flush();
+            csv.close();
+        } catch (final IOException e) {
+            return "ERROR: Failed to create CSV output";
+        }
+        return outs.toString();
+    }
+
     private String concatenateTapes(final BulkObjectList objects) {
         if(Guard.isNullOrEmpty(objects.getObjects())) {
             return "No Physical Placement";
