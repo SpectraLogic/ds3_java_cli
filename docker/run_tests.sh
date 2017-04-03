81adeeda08bcfbb890ceb8cc31a60ffd8861492f
DenverM80 <denverm@spectralogic.com>
2017-04-03 12:41:30 -0600

    print the current git rev hash before running tests in docker container

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -6,19 +6,18 @@ echo DS3_ENDPOINT ${DS3_ENDPOINT}
 echo DS3_SECRET_KEY ${DS3_SECRET_KEY}
 echo DS3_ACCESS_KEY ${DS3_ACCESS_KEY}
 
-echo "cd /opt"
+set -x
+
 cd /opt
 
 if [ ${GIT_BRANCH} != "master" ]; then
-  echo git clone ${GIT_REPO} --branch ${GIT_BRANCH} --single-branch
   git clone ${GIT_REPO} --branch ${GIT_BRANCH} --single-branch
 else
-  echo git clone ${GIT_REPO}
   git clone ${GIT_REPO}
 fi
 
-echo "cd ds3_java_cli"
 cd ds3_java_cli
+git rev-parse HEAD
 
 # Build all projects, run all unit tests, exclude certification integration tests.
 ./gradlew test -x ds3-cli-certification:test
