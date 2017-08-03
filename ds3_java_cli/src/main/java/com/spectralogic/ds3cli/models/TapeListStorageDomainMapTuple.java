b5e828875e2c71575bd84e84bb33fe13e8853000
GraciesPadre <ShastasPadre1@gmail.com>
2017-08-03 17:33:01 -0600

    This is the second part of a request to add bucket option to the GetTapes command and to add the “Storage Domain” output parameter to the results.

--- ds3_java_cli/src/main/java/com/spectralogic/ds3cli/models/TapeListStorageDomainMapTuple.java
+++ ds3_java_cli/src/main/java/com/spectralogic/ds3cli/models/TapeListStorageDomainMapTuple.java
@@ -20,17 +20,20 @@ import com.spectralogic.ds3client.models.TapeList;
 import java.util.Map;
 import java.util.UUID;
 
-public class GetTapesResult implements Result<TapeListStorageDomainMapTuple> {
+public class TapeListStorageDomainMapTuple {
+    private final TapeList tapeList;
+    private final Map<UUID, String> storageDomainIdNameMap;
 
-    private final TapeListStorageDomainMapTuple tapeListStorageDomainMapTuple;
-
-    public GetTapesResult(final TapeList tapes, final Map<UUID, String> storageDomainIdNameMap) {
-        this.tapeListStorageDomainMapTuple = new TapeListStorageDomainMapTuple(tapes, storageDomainIdNameMap);
+    public TapeListStorageDomainMapTuple(final TapeList tapeList, final Map<UUID, String> storageDomainIdNameMap) {
+        this.tapeList = tapeList;
+        this.storageDomainIdNameMap = storageDomainIdNameMap;
     }
 
-    @Override
-    public TapeListStorageDomainMapTuple getResult() {
-        return tapeListStorageDomainMapTuple;
+    public TapeList getTapeList() {
+        return tapeList;
+    }
+
+    public Map<UUID, String> getStorageDomainIdNameMap() {
+        return storageDomainIdNameMap;
     }
 }
-
--- ds3_java_cli/src/main/java/com/spectralogic/ds3cli/views/json/GetTapesView.java
+++ ds3_java_cli/src/main/java/com/spectralogic/ds3cli/views/json/GetTapesView.java
@@ -18,16 +18,17 @@ package com.spectralogic.ds3cli.views.json;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.spectralogic.ds3cli.View;
 import com.spectralogic.ds3cli.models.GetTapesResult;
+import com.spectralogic.ds3cli.models.TapeListStorageDomainMapTuple;
 import com.spectralogic.ds3cli.util.JsonMapper;
-import com.spectralogic.ds3client.models.TapeList;
+import com.spectralogic.ds3client.utils.Guard;
 
 public class GetTapesView implements View<GetTapesResult> {
     @Override
     public String render(final GetTapesResult obj) throws JsonProcessingException {
-        final TapeList result = obj.getResult();
+        final TapeListStorageDomainMapTuple result = obj.getResult();
         final CommonJsonView view = CommonJsonView.newView(CommonJsonView.Status.OK);
 
-        if (result == null || null == result.getTapes()) {
+        if (result == null || null == result.getTapeList() || Guard.isNullOrEmpty(result.getTapeList().getTapes())) {
             view.message("You do not have any tapes");
             return JsonMapper.toJson(view);
         }
