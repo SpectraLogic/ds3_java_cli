307fbe9129c977ce6514779400bb0088c5781a5e
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2023-09-05 16:12:26 -0600

    Upgrade gradle plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@
  */
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention").version("0.5.0")
+    id("org.gradle.toolchains.foojay-resolver-convention").version("0.7.0")
 }
 
 rootProject.name = "ds3_java_cli"
